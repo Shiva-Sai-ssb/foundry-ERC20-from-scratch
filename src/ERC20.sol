af3f28771c78f9a34a7dcb18d7545f28d184ec44
Shiva Sai <112751524+Shiva-Sai-ssb@users.noreply.github.com>
2025-10-31 16:52:36 +0530

    chore: add NatSpec comments to ERC20 contract

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -21,19 +21,23 @@ contract ERC20 {
     uint256 private constant MAX_SUPPLY = 1_000_000 * 10 ** 18;
 
     mapping(address => uint256) private s_balances;
-    // Owner => Spender => allowance
     mapping(address => mapping(address => uint256)) private s_allowances;
 
     // Events
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
+    /// @notice Sets the name and symbol of the token, and sets deployer as owner
+    /// @param _name Token name
+    /// @param _symbol Token symbol
     constructor(string memory _name, string memory _symbol) {
         s_name = _name;
         s_symbol = _symbol;
         i_owner = msg.sender;
     }
 
+    /// @notice Throws an error if the caller is not the owner
+    /// @dev This modifier is used to restrict access to certain functions(like minting)
     modifier onlyOwner() {
         if (msg.sender != i_owner) {
             revert ERC20__NotOwner();
@@ -41,7 +45,12 @@ contract ERC20 {
         _;
     }
 
-    // Public Functions
+    /// @notice Mints new tokens to a specified address
+    /// @param _to The address to mint tokens to
+    /// @param _amount The amount of tokens to mint
+    /// @dev Only the owner can mint tokens
+    /// @dev Reverts if trying to mint to the zero address or if the total supply exceeds MAX_SUPPLY
+    /// @dev Emits a Transfer event with the `from` address set to zero
     function mint(address _to, uint256 _amount) public onlyOwner {
         if (_to == address(0)) {
             revert ERC20__MintToZeroAddress();
@@ -56,6 +65,10 @@ contract ERC20 {
         emit Transfer(address(0), _to, _amount);
     }
 
+    /// @notice Burns tokens from the caller's balance
+    /// @param _amount The amount of tokens to burn
+    /// @dev Reverts if the caller's balance is insufficient
+    /// @dev Emits a Transfer event with the `to` address set to zero
     function burn(uint256 _amount) public {
         if (s_balances[msg.sender] < _amount) {
             revert ERC20__InsufficientBalance();
@@ -66,6 +79,11 @@ contract ERC20 {
         emit Transfer(msg.sender, address(0), _amount);
     }
 
+    /// @notice Transfers tokens from the caller to a specified address
+    /// @param _to The address to transfer tokens to
+    /// @param _amount The amount of tokens to transfer
+    /// @dev Reverts if the `_to` address is zero or if the caller's balance is insufficient
+    /// @dev Emits a Transfer event, returning true on success
     function transfer(address _to, uint256 _amount) public returns (bool) {
         if (_to == address(0)) {
             revert ERC20__TransferToZeroAddress();
@@ -81,6 +99,11 @@ contract ERC20 {
         return true;
     }
 
+    /// @notice Approves a spender to spend a specified amount of tokens on behalf of the caller
+    /// @param _spender The address to approve
+    /// @param _amount The amount of tokens to approve
+    /// @dev Reverts if the `_spender` address is zero
+    /// @dev Emits an Approval event, returning true on success
     function approve(address _spender, uint256 _amount) public returns (bool) {
         if (_spender == address(0)) {
             revert ERC20__ApproveToZeroAddress();
@@ -91,6 +114,13 @@ contract ERC20 {
         return true;
     }
 
+    /// @notice Transfers tokens from one address to another on behalf of the owner
+    /// @param _from The address to transfer tokens from
+    /// @param _to The address to transfer tokens to
+    /// @param _amount The amount of tokens to transfer
+    /// @dev Reverts if the `_from` or `_to` address is zero, if the `_from` address has insufficient balance,
+    /// or if the caller does not have enough allowance
+    /// @dev Emits a Transfer event, returning true on success
     function transferFrom(address _from, address _to, uint256 _amount) public returns (bool) {
         if (_from == address(0)) {
             revert ERC20__TransferFromZeroAddress();
@@ -115,6 +145,11 @@ contract ERC20 {
         return true;
     }
 
+    /// @notice Increases the allowance of a spender
+    /// @param _spender The address to increase the allowance for
+    /// @param _amount The amount to increase the allowance by
+    /// @dev Reverts if the `_spender` address is zero
+    /// @dev Emits an Approval event, returning true on success
     function increaseAllowance(address _spender, uint256 _amount) public returns (bool) {
         if (_spender == address(0)) {
             revert ERC20__ApproveToZeroAddress();
@@ -125,6 +160,11 @@ contract ERC20 {
         return true;
     }
 
+    /// @notice Decreases the allowance of a spender
+    /// @param _spender The address to decrease the allowance for
+    /// @param _amount The amount to decrease the allowance by
+    /// @dev Reverts if the `_spender` address is zero or if the current allowance is less than the amount to decrease
+    /// @dev Emits an Approval event, returning true on success
     function decreaseAllowance(address _spender, uint256 _amount) public returns (bool) {
         if (_spender == address(0)) {
             revert ERC20__ApproveToZeroAddress();
@@ -140,35 +180,48 @@ contract ERC20 {
         return true;
     }
 
-    // View Functions
+    /// @notice Returns the name of the token
     function name() public view returns (string memory) {
         return s_name;
     }
 
+    /// @notice Returns the symbol of the token
     function symbol() public view returns (string memory) {
         return s_symbol;
     }
 
+    /// @notice Returns the number of decimals the token uses
     function decimals() public pure returns (uint8) {
         return DECIMALS;
     }
 
+    /// @notice Returns the address of the contract owner
     function owner() public view returns (address) {
         return i_owner;
     }
 
+    /// @notice Returns the total supply of tokens
     function totalSupply() public view returns (uint256) {
         return s_totalSupply;
     }
 
+    /// @notice Returns the maximum supply of tokens
     function maxSupply() public pure returns (uint256) {
         return MAX_SUPPLY;
     }
 
+    /// @notice Returns the balance of a specific account
+    /// @param _account The address to check the balance of
+    /// @return The balance of the specified account
     function balanceOf(address _account) public view returns (uint256) {
         return s_balances[_account];
     }
 
+    /// @notice Returns the allowance of a spender for a specific owner
+    /// @param _owner The address of the owner
+    /// @param _spender The address of the spender
+    /// @return The amount of tokens the spender is allowed to spend on behalf of the owner
+    /// @dev Returns zero if the owner or spender has no allowance set
     function allowance(address _owner, address _spender) public view returns (uint256) {
         return s_allowances[_owner][_spender];
     }
