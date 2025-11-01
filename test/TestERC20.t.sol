e402712c9a462ce36eb7c1e9ec2f89bb627c1327
Shiva Sai <112751524+Shiva-Sai-ssb@users.noreply.github.com>
2025-11-01 11:40:16 +0530

    feat: add tests for mint nd burn

--- test/TestERC20.t.sol
+++ test/TestERC20.t.sol
@@ -14,6 +14,9 @@ contract TestERC20 is Test {
     address USER3 = makeAddr("user3");
     uint256 public constant STARTING_VALUE = 5 ether;
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
     function setUp() external {
         deployer = new DeployERC20();
         erc20 = deployer.run();
@@ -62,5 +65,91 @@ contract TestERC20 is Test {
         assertEq(erc20.allowance(OWNER, USER1), 0);
         assertEq(erc20.allowance(USER1, OWNER), 0);
         assertEq(erc20.allowance(USER2, USER3), 0);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           MINT FUNCTION TESTS
+    //////////////////////////////////////////////////////////////*/
+
+    function testOnlyOwnerCanCallMintFunction() external {
+        vm.prank(USER1);
+        vm.expectRevert(ERC20.ERC20__NotOwner.selector);
+        erc20.mint(USER1, 100);
+    }
+
+    function testCannotMintToZeroAddress() external {
+        vm.prank(OWNER);
+        vm.expectRevert(ERC20.ERC20__MintToZeroAddress.selector);
+        erc20.mint(address(0), 100);
+    }
+
+    function testTotalSupplyDoesNotExceedMaxSupply() external {
+        uint256 maxSupply = erc20.maxSupply();
+
+        vm.prank(OWNER);
+        vm.expectRevert(ERC20.ERC20__MaxSupplyExceeded.selector);
+        erc20.mint(USER1, maxSupply + 1);
+    }
+
+    function testBalanceOfRecipientIncreasesAfterMinting() external {
+        uint256 balanceBefore = erc20.balanceOf(USER1);
+        vm.prank(OWNER);
+        erc20.mint(USER1, 100);
+        assertEq(erc20.balanceOf(USER1), balanceBefore + 100);
+    }
+
+    function testTotalSupplyIncreasesAfterMinting() external {
+        uint256 supplyBefore = erc20.totalSupply();
+        vm.prank(OWNER);
+        erc20.mint(USER1, 1000);
+        assertEq(erc20.totalSupply(), supplyBefore + 1000);
+    }
+
+    function testMintingFunctionEmitsTransferEvent() external {
+        vm.prank(OWNER);
+        vm.expectEmit(true, true, false, true, address(erc20));
+        emit Transfer(address(0), USER1, 100);
+        erc20.mint(USER1, 100);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           BURN FUNCTION TESTS
+    //////////////////////////////////////////////////////////////*/
+
+    function testAnyoneCanBurnTheirOwnTokens() external {
+        vm.prank(OWNER);
+        erc20.mint(USER1, 200);
+
+        vm.prank(USER1);
+        erc20.burn(100);
+        assertEq(erc20.balanceOf(USER1), 100);
+    }
+
+    function testBurnFunctionChecksIfCallerHasEnoughBalance() external {
+        vm.prank(USER1);
+        vm.expectRevert(ERC20.ERC20__InsufficientBalance.selector);
+        erc20.burn(100);
+    }
+
+    function testBalanceAndTotalSupplyUpdateCorrectlyAfterBurning() external {
+        vm.prank(OWNER);
+        erc20.mint(USER1, 200);
+
+        uint256 supplyBefore = erc20.totalSupply();
+        vm.prank(USER1);
+        erc20.burn(100);
+
+        assertEq(erc20.balanceOf(USER1), 100);
+        assertEq(erc20.totalSupply(), supplyBefore - 100);
+    }
+
+    function testBurnFunctionEmitsTransferEvent() external {
+        vm.prank(OWNER);
+        erc20.mint(USER1, 100);
+
+        vm.prank(USER1);
+        vm.expectEmit(true, true, false, true, address(erc20));
+        emit Transfer(USER1, address(0), 50);
+        erc20.burn(50);
     }
 }
